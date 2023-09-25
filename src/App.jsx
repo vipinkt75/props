01eff75896b37b15e601975a04471e7097685a36
vipinkt75 <vipinktvipinkt5@gmail.com>
2023-09-25 11:17:08 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   
 
   return (
-  <div>
+  <div className='card-total'>
     <Card title="Accordion" subtitle="Close button" discription="Some quick example text to build on the card title and make up the bulk of the card's content."/>
     <Card title="Alerts" subtitle="Collapse"  discription="With supporting text below as a natural lead-in to additional content."/>
     <Card title="Badge" subtitle="Dropdowns"  discription="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."/>
